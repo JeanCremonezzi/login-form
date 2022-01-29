43da1f8e1331251539f3ef49b80bd075e42aecd0
JeanCremonezzi <jeancremonezzi@hotmail.com>
2022-01-29 15:33:13 -0300

    feat: Initial structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<main className="card">
+			<section className="signin">
+        <h1>Sign in</h1>
+
+				<form>
+					<label htmlFor="inpt-signin-username">Username</label>
+					<input
+						type="text"
+						name="inpt-signin-username"
+						placeholder="Your Username"
+					/>
+
+					<label htmlFor="inpt-signin-password">Password</label>
+					<input
+						type="password"
+						name="inpt-signin-password"
+						placeholder="Your Password"
+					/>
+
+					<button type="submit">Sign in</button>
+				</form>
+			</section>
+
+			<section className="signup">
+        <h1>Sign up</h1>
+
+				<form>
+					<label htmlFor="inpt-signup-username">Username</label>
+					<input
+						type="text"
+						name="inpt-signup-username"
+						placeholder="Your Username"
+					/>
+
+					<label htmlFor="inpt-signup-email">Email</label>
+					<input
+						type="email"
+						name="inpt-signup-email"
+						placeholder="youremail@mail.com"
+					/>
+
+					<label htmlFor="inpt-signup-password">Password</label>
+					<input
+						type="password"
+						name="inpt-signup-password"
+						placeholder="Your Password"
+					/>
+
+					<button type="submit">Sign up</button>
+				</form>
+			</section>
+		</main>
+	);
 }
 
 export default App;
